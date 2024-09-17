5d8dc5b79866adad45f9ae1635099052cafeeddc
Lara Carvalho <laraftoller@gmail.com>
2024-09-17 15:44:34 -0300

    feat: evaluate base types and some expressions

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/laracarvalho/ino-lang/evaluator"
 	"github.com/laracarvalho/ino-lang/lexer"
 	"github.com/laracarvalho/ino-lang/parser"
 )
@@ -31,8 +32,12 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+
 	}
 }
 
